dbc5e7aa3b2edbebcfae749c89779e1c2ea2a6b1
bruno-dv2 <brunoge3@edu.unifor.br>
2024-09-26 15:12:51 -0300

    Implementa config e index

---
+++ Config/config.php
@@ -0,0 +1,12 @@
+<?php 
+
+define("ROOT_PATH", __DIR__ . "/../");
+
+define("DATABASE_FILE", ROOT_PATH . 'database.json');
+
+require_once ROOT_PATH . "Controller/BaseController.php";
+
+require_once ROOT_PATH . "Model/UserModel.php";
+
+?>
+
